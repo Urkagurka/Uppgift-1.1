ff7bb32e92711bf67e183db28d00200e55279c6e
alve06001 <alve06001@E-PW0234LA>
2023-09-06 10:49:17 +0200

    Add project files.

---
+++ Uppgift 1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;                 //using system betyder att du använder ett system
+                              
+namespace Uppgift_1._1        //namespace är en samling för en kåd som handlar om samma sak
+{                             
+    class Program             //class Program är en samling av saker som du ska anvädna
+    {
+        static void Main(string[] args)                                                //Det associseras med klassen av något
+        {                                                                              
+            Console.WriteLine("Du är nästan sklar med kapitel 1.");                    //vad du vill få den att skriva ut på skärmen 
+            Console.WriteLine("Du har lärt dig skriva ut tecken som  \" och \\ ");     //vad du vill få den att skriva ut på skärmen 
+        }
+    }
+}
